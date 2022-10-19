8849cf919b438142203c8de45aabda25f42c627d
Greg Uezono <greg.uezono@gmail.com>
2022-10-19 16:10:24 -0300

    [MC-269] Fix eslint airbnb errors

--- src/helpers/buildMap.ts
+++ src/helpers/buildMap.ts
@@ -5,15 +5,14 @@ const supportedChains = [1, 10, 137, 42161];
 
 function buildMap() {
   const chainTokenMap = new TSMap<any, any>();
-  for (const supportedChain of supportedChains) {
-    chainTokenMap.set(supportedChain, new TSMap<any, any>());
-  }
-  for (const token of tokenList.tokens) {
+  supportedChains.forEach((chainId) => chainTokenMap.set(chainId, new TSMap<any, any>()));
+
+  tokenList.tokens.forEach((token) => {
     if (supportedChains.includes(token.chainId)) {
       const tokenMap = chainTokenMap.get(token.chainId);
       tokenMap.set(token.address, token);
     }
-  }
+  });
   return chainTokenMap;
 }
 
