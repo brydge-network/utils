6391aa75f379cd328de27fd1cce2d87f1e2a59df
Ben Macnaughton <robert.macnaughton@mail.mcgill.ca>
2022-11-07 13:17:45 -0500

    Change tests

--- src/createCalls/getTotalSupply.ts
+++ src/createCalls/getTotalSupply.ts
@@ -1,16 +1,13 @@
 import { minterABIs } from './minterABIs';
 import { Contract, ethers } from 'ethers';
 
-export function getTotalSupply(minterAddress: string, jsonRpcUrl: string) {
+export async function getTotalSupply(minterAddress: string, jsonRpcUrl: string) {
   const minterABI = minterABIs[minterAddress];
   if (!minterABI) {
     throw new Error('Minter ABI not found');
   }
   const provider = new ethers.providers.JsonRpcProvider(jsonRpcUrl);
   const minter = new Contract(minterAddress, minterABI, provider);
-  const supply = async function () {
-    const totalSupply = await minter.totalSupply();
-    return totalSupply;
-  };
-  return supply();
+  const totalSupply = await minter.totalSupply();
+  return totalSupply;
 }
