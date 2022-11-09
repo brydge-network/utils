cbaa2907b05fb868de4bc75bc1ffb73fc62779f4
Greg Uezono <greg.uezono@gmail.com>
2022-11-09 18:31:43 -0500

    [MC-314] Rename to layerZero

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 export * from './brydge';
 export * from './chains';
 export * from './tokens';
-export * from './layerzero';
+export * from './layerZero';
 export * from './quickswap';
 
 // export { brydge, chains, tokens };
