624a677ca5c1c106ac6f591245729c8d825227b3
Ben Macnaughton <robert.macnaughton@mail.mcgill.ca>
2022-11-22 13:44:13 -0500

    Removimg unused constants

--- test/unit/createCalls.test.ts
+++ test/unit/createCalls.test.ts
@@ -1,25 +1,4 @@
 import { createMintICall } from '../../src/createCalls';
-import { ICall } from '../../src/constants';
-
-const darkMode = true;
-const widgetMode = 'PURCHASE';
-const outputTokenAddress = '0x0000000000000000000000000000000000000000';
-const destinationChainId = 1;
-const title = 'title';
-const price = 100;
-const iCalls: ICall[] = [
-  {
-    _to: '0x0000000000000000000000000000000000000000',
-    _value: 0,
-    _calldata: '0x0000000000000000000000000000000000000000000000000000000000000000',
-  },
-  {
-    _to: '0x0000000000000000000000000000000000000000',
-    _value: 0,
-    _calldata: '0x0000000000000000000000000000000000000000000000000000000000000000',
-  },
-];
-const baseColor = '#000000';
 
 describe('utils | safePackageName', () => {
   it('should create icalls using an integrated contact address', () => {
