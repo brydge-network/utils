269e0accd891abd7e3262bf9673ca6f8b46e7068
Jeff Weiner <jeffantonweiner@gmail.com>
2023-01-10 17:01:54 -0800

    add hasInteractedWith function

--- src/dataCollector.ts
+++ src/dataCollector.ts
@@ -23,6 +23,66 @@ type Result = {
   returnData: string
 };
 
+const blockExplorerURL: { [chainId: number]: string } = {
+  1: 'https://api.etherscan.io/api?',
+  10: 'https://api-optimistic.etherscan.io/api?',
+  137: 'https://api.polygonscan.com/api?',
+  42161: 'https://api.arbiscan.io/api?',
+  43114: 'https://api.snowtrace.io/api?',
+};
+
+function getTopRecentUsers(addresses: string[], amount: number): string[] {
+  const frequencies: { [key: string]: number } = {};
+  for (const address of addresses) {
+    if (frequencies[address] === undefined) {
+      frequencies[address] = 1;
+    } else {
+      frequencies[address] += 1;
+    }
+  }
+
+  const frequencyArray: [number, string][] = [];
+  for (const key in frequencies) {
+    frequencyArray.push([frequencies[key], key]);
+  }
+
+  // Sort in descending order with frequency as key
+  frequencyArray.sort((a, b) => b[0] - a[0]);
+
+  // Get most frequent element out of array
+  const mostFrequent = [];
+  for (let i = 0; i < amount; i += 1) {
+    mostFrequent.push(frequencyArray[i][1]);
+  }
+
+  return mostFrequent;
+}
+
+async function hasInteractedWith(user: string, contract: string, chainId: number) {
+  const params = {
+    module: 'account',
+    action: 'txlist',
+    address: user,
+    startblock: '0',
+    endblock: '99999999',
+    page: '1',
+    offset: '1000',
+    sort: 'desc',
+    apikey: '2RMD4DS4M3871Y1FCBPFBRW7I56V5VZP44',
+  };
+  const uriComponent = new URLSearchParams(params).toString();
+  const finalURL = `${blockExplorerURL[chainId]}${uriComponent}`;
+
+  const uniqueAddresses = new Set<string>();
+  console.log('finalURL', finalURL);
+  await axios.get(finalURL).then(async (res) => {
+    const addresses = res.data.result.map((tx: any) => tx.to);
+    addresses.forEach((item: string) => uniqueAddresses.add(item));
+  });
+
+  return uniqueAddresses.has(contract);
+}
+
 const checkTokenBalances = async ({ account, provider, chainId }: CheckTokenBalancesArgs) => {
   const tokens = tokensList.tokens.filter((token) => token.chainId === chainId).map((token) => token.address);
   const multicallAddress = MULTICALL_ADDRESSES[chainId];
@@ -38,9 +98,9 @@ const checkTokenBalances = async ({ account, provider, chainId }: CheckTokenBala
     gasLimit: 30_000_000,
   }));
 
-  const [blockNumber, returnData] = (await multicall.multicall(calls)) as [BigNumber, Result[]];
+  const [blockNumber, rData] = (await multicall.multicall(calls)) as [BigNumber, Result[]];
 
-  const balances = returnData.map(({ /* gasUsed, success, */ returnData }): BigNumber => {
+  const balances = rData.map(({ /* gasUsed, success, */ returnData }): BigNumber => {
     const decoded = iface.decodeFunctionResult('balanceOf', returnData);
     return decoded[0];
   });
@@ -76,7 +136,10 @@ export async function runAnalysis(testChainId: SupportedChainId) {
     const addresses = res.data.result.map((tx: any) => tx.from);
     // console.log('addresses', addresses);
     uniqueAddresses = [...new Set(addresses)];
-    console.log('uniqueAddresses', uniqueAddresses);
+    const topAddresses = getTopRecentUsers(addresses, 10);
+    console.log('uniqueAddresses: ', uniqueAddresses);
+    console.log('topAddresses: ', topAddresses);
+    console.log('hasInteractedWith: ', await hasInteractedWith('0x6bC6B0466b0a3bD06ab3018ca609a07Fe7E04a37', '0x46171bf797c4ffee613e66b64a4264534fef180a', 137));
     const narrowedList = uniqueAddresses.slice(0, 100);
     // const { blockNumber, balances } = await checkTokenBalances({
     //   tokens,
@@ -99,7 +162,7 @@ export async function runAnalysis(testChainId: SupportedChainId) {
       chainId: testChainId,
     });
 
-    console.log('balances lenght', balances.length);
+    console.log('balances length', balances.length);
     // console.log('balancesResult', balances);
     let sum = 0;
     balances.forEach((value) => {
